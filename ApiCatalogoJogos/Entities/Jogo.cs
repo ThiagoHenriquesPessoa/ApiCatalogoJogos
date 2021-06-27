8781ef6772f6c62138795963eca04938e930d13d
Thiago Henriques <thiagokxl5@gmail.com>
2021-06-27 00:13:19 -0300

    Entities, Exception e Repositories

--- ApiCatalogoJogos/Entities/Jogo.cs
+++ ApiCatalogoJogos/Entities/Jogo.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ApiCatalogoJogos.ViewModel
+namespace ApiCatalogoJogos.Entities
 {
-    public class JogoViewModel
+    public class Jogo
     {
         public Guid Id { get; set; }
         public string Nome { get; set; }
         public string Produtora { get; set; }
-        public double Preco { get; set; }        
+        public double Preco { get; set; }
     }
 }
---
+++ ApiCatalogoJogos/Exceptions/JogoNaoCasdastradoException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoJogos.Exceptions
+{
+    public class JogoNaoCasdastradoException : Exception
+    {
+        public JogoNaoCasdastradoException() : base("Este jogo já está cadastrado") { }
+    }
+}
