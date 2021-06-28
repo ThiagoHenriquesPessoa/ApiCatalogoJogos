d6114bba9bf1406c29708575540b09dd87eda8a5
Thiago Henriques <thiagokxl5@gmail.com>
2021-06-28 10:08:58 -0300

    Conecxao com banco de dados e outras implementacoes

--- ApiCatalogoJogos/Services/JogoService.cs
+++ ApiCatalogoJogos/Services/JogoService.cs
@@ -97,7 +97,7 @@ namespace ApiCatalogoJogos.Services
         }
         public async Task Remover(Guid id)
         {
-            var jogo = _jogoRepository.Obter(id);
+            var jogo = await _jogoRepository.Obter(id);
 
             if (jogo == null) throw new JogoJaCadastradoException();
 
